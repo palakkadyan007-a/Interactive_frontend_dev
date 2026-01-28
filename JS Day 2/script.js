acb98b4e884cc73933284996adb5891ef2682439
palakkadyan007-a <palakkadyan007@gmail.com>
2026-01-28 12:56:34 +0530

    INitial commit

---
+++ JS Day 2/script.js
@@ -0,0 +1,22 @@
+// map maethod
+// let arr = [1,2,3,4,5]
+
+// let result = arr.map((x)=>x)
+// console.log(result);
+
+// // filter method
+// let score = [12,5,7,22,34,6];
+// let highscore = score.filter((score)=>{
+//     if (score)>=15 {console.log(score, "pass");}
+//     else {console.log(score, "fail");}
+// })
+
+let number = [1,3,4]
+for (let i=0; i<number.length; i++){
+    console.log(number[i] + number[i+1]);
+}
+
+
+
+
+// reduce method
