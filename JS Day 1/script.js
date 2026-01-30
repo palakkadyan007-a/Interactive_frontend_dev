dc4d6f3c57f4854f5c3dfb09c5c46bb6362c83a0
palakkadyan007-a <palakkadyan007@gmail.com>
2026-01-30 20:36:16 +0530

    Change function calls to use 'Palak' and add sample
    Updated function calls to use 'Palak' instead of 'Piyush' and added a sample function call.

--- JS Day 1/script.js
+++ JS Day 1/script.js
@@ -6,13 +6,13 @@ function greet(name) {
     console.log(name);
 }
 
-greet("Piyush");
+greet("Palak");
 
 // _arrow function 
 
 const farewell = (name) => { console.log(name); };
 
-farewell("Piyush");
+farewell("Palak");
 
 // _callback function => a function passed as an argument to another function
 
@@ -24,5 +24,6 @@ const sample = (callback) => {
 const demo = () => {
     console.log("Demo");
 };
+
 
 sample(demo);
